49f8ac69103fe99b738a96e8bc19063749dfc52a
Nikolaeva Darya <dashapavlova@yandex.ru>
2022-02-05 01:55:10 +0300

    Вносит самые последник исправления

--- source/js/script.js
+++ source/js/script.js
@@ -3,18 +3,18 @@ var navToggle = document.querySelector('.main-nav__toggle');
 var pageHeader = document.querySelector('.page-header');
 
 navMain.classList.remove('main-nav--nojs');
-navMain.classList.add('main-nav--closed');
-navMain.classList.remove('main-nav--opened');
-pageHeader.classList.remove('page-header--opened');
+pageHeader.classList.remove('page-header--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    pageHeader.classList.remove('page-header--closed');
     pageHeader.classList.add('page-header--opened');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    pageHeader.classList.add('page-header--closed');
     pageHeader.classList.remove('page-header--opened');
   }
 });
